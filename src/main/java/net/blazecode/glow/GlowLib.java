1c24426a9b121093963fd8bce9216eef863ffb36
BlazeCodeNet <84934108+BlazeCodeNet@users.noreply.github.com>
2022-02-25 01:32:51 -0600

    Removed unused libs for cloth config.

--- src/main/java/net/blazecode/glow/GlowLib.java
+++ src/main/java/net/blazecode/glow/GlowLib.java
@@ -1,10 +1,5 @@
 package net.blazecode.glow;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 import net.blazecode.glow.commands.GlowCommand;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
@@ -17,13 +12,12 @@ import java.util.logging.Logger;
 public class GlowLib implements DedicatedServerModInitializer
 {
 	public static final String MODID = "glow";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 	public static final Logger LOGGER = Logger.getLogger( MODID );
 
 	@Override
 	public void onInitializeServer( )
 	{
-		AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
 		CommandRegistrationCallback.EVENT.register( ( dispatcher, dedicated) ->
 		{
 			if(dedicated)
@@ -31,23 +25,5 @@ public class GlowLib implements DedicatedServerModInitializer
 				GlowCommand.register(dispatcher);
 			}
 		});
-	}
-
-	public static ModConfig getConfig()
-	{
-		if (config == null)
-		{
-			config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-		}
-		return config;
-	}
-
-	private static ModConfig config;
-
-	@Config(name = MODID)
-	public static class ModConfig implements ConfigData
-	{
-		@Comment("Toggles the entire mod on or off - NOT IMPLEMENTED YET!")
-		boolean enabled = true;
 	}
 }
